db3cd2c2f72c87d41aa6fe8a6df41777bf489f6b
itamar-reiter <itamarreiter@gmail.com>
2025-09-30 00:51:01 +0300

    edit coonfig.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  // If you're using image optimization, you may need:
+  images: {
+    unoptimized: true,
+  },
+}
+
+module.exports = nextConfig
 
 export default nextConfig;
