adc6c07851b30a24528a06f78b958471916b6695
itamar-reiter <itamarreiter@gmail.com>
2025-09-30 08:44:00 +0300

    changed rendering component logic

--- app/page.js
+++ app/page.js
@@ -8,24 +8,16 @@ import { CurrentPage, sampleSongs } from "../utils/utils";
 import Closure from "./closure/closure";
 
 export default function Home() {
-
-
-
-
-
-
-
   
   const [currentPage, setCurrentPage] = useState(CurrentPage.USER_INPUT);
+  
   const [loaderHeadline, setLoaderHeadline] = useState("");
-
   const [loaderRedirect, setLoaderRedirect] = useState(CurrentPage.USER_INPUT);
 
   const onUserInputSubmit = (userInput) => {
     setLoaderHeadline("searching for songs right for your mood...");
     setLoaderRedirect(CurrentPage.USER_CHOOSE_SONGS);
     setCurrentPage(CurrentPage.LOADER);
-
   }
 
   const onEscapeLoader = () => {
@@ -42,23 +34,13 @@ export default function Home() {
     setCurrentPage(CurrentPage.USER_INPUT);
   }
 
-  const renderCurrentComponent = () => {
-    switch(currentPage) {
-      case CurrentPage.LOADER:
-        return <Loader headline={loaderHeadline} escapeLoader={onEscapeLoader} />;
-      case CurrentPage.USER_INPUT:
-        return <UserInput onSubmit={onUserInputSubmit} />;
-      case CurrentPage.USER_CHOOSE_SONGS:
-        return <UserChooseSongs  songs={sampleSongs} onSubmit={onUserChooseSongsSubmit}/>;
-      case CurrentPage.CLOSURE:
-        return <Closure onRestart={onRestart} />
-    }
-  }
-
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        {renderCurrentComponent()}
+        {currentPage === CurrentPage.LOADER && <Loader headline={loaderHeadline} escapeLoader={onEscapeLoader} />}
+        {currentPage === CurrentPage.USER_INPUT && <UserInput onSubmit={onUserInputSubmit} />}
+        {currentPage === CurrentPage.USER_CHOOSE_SONGS && <UserChooseSongs  songs={sampleSongs} onSubmit={onUserChooseSongsSubmit}/>}
+        {currentPage === CurrentPage.CLOSURE && <Closure onRestart={onRestart} />}
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         @ Renana & Itamar || Experis SW82
